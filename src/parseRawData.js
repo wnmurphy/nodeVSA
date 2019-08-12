f30ddc3e9d3666bf87aac918c3e4e6c735c0e88f
Neil Murphy <wnmurphy@gmail.com>
2019-08-12 10:33:44 -0700

    Add 'final' flag to each day in parseRawData.js

--- src/parseRawData.js
+++ src/parseRawData.js
@@ -5,18 +5,21 @@ function parseRawData (stock) {
   try {
     const transformed = [];
     const timeSeries = stock['Time Series (Daily)'];
+    const now = new Date();
     if (!timeSeries) {
       throw stock;
     }
     for (const date in timeSeries) {
-      const dayOfData = {};
-      dayOfData.date = date;
-      dayOfData.h = parseFloat(timeSeries[date]['2. high']);
-      dayOfData.l = parseFloat(timeSeries[date]['3. low']);
-      dayOfData.c = parseFloat(timeSeries[date]['4. close']);
-      dayOfData.v = parseFloat(timeSeries[date]['5. volume']);
-      dayOfData.pivotHigh = false;
-      dayOfData.pivotLow = false;
+      const dayOfData = {
+        date,
+        h: parseFloat(timeSeries[date]['2. high']),
+        l: parseFloat(timeSeries[date]['3. low']),
+        c: parseFloat(timeSeries[date]['4. close']),
+        v: parseFloat(timeSeries[date]['5. volume']),
+        pivotHigh: false,
+        pivotLow: false,
+        final: now > new Date(date) ? true : false, // TODO: refine this to after 1pm
+      };
       transformed.push(dayOfData); 
     }
     // Reverse array so the most recent day is at last index.
